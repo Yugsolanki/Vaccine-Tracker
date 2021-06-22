c0dddd09212ba97efb0ec5354db0545c1aaefd35
Yugsolanki <yugsolanki15@gmail.com>
2021-06-22 22:01:42 +0530

    no vaccine alert

--- main.js
+++ main.js
@@ -23,24 +23,30 @@ function getDate() {
 getDate();
 
 function main() {
-  
+
+var sessionsCount = {};
 var sessions = {};
+
 
 fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${locationNode.value}&date=${date}`)
   .then(response => {return response.json()})
   .then(res => {
-    sessions = res.sessions;
+    sessionsCount = res.sessions;
   })
   
+  if (sessionsCount.length>0) {
+    sessions = sessionsCount
+  } else {
+    alert('Sorry no vaccine slots are available on this pincode, age limit and date.')
+  }
 //sessions[i]
 
 setTimeout(()=>{
   
   for (var i = 0; i < sessions.length; i++) {
-    
     let {name, address, min_age_limit: age, fee, available_capacity_dose1: dose1, available_capacity_dose2: dose2} = sessions[i];
     
-    if (age==ageSelectNode.value && (dose1>=0 || dose2>=0)) {
+    if (age==ageSelectNode.value && (dose1>0 || dose2>0)) {
       
       class data {
         constructor(name, address, age, fee, dose1, dose2) {
