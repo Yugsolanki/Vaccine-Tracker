3f1984bd50d2f3bbec28472d8d5a88f563c6bc25
Yugsolanki <yugsolanki15@gmail.com>
2021-06-22 23:31:36 +0530

    more Location

--- main.js
+++ main.js
@@ -28,31 +28,30 @@ var sessionsCount = {};
 var sessions = {};
 
 
-fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${locationNode.value}&date=${date}`)
+fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${locationNode.value}&date=23-06-2021`)
   .then(response => {return response.json()})
   .then(res => {
     sessionsCount = res.sessions;
   })
-  
   setTimeout(()=>{
     if (sessionsCount.length>0) {
       sessions = sessionsCount
     } else {
       alert('Sorry no vaccine slots are available on this pincode, age limit and date.')
     }
-  }, 100)
+  }, 1000)
   
 //sessions[i]
 
 setTimeout(()=>{
   
   for (var i = 0; i < sessions.length; i++) {
-    let {name, address, min_age_limit: age, fee, available_capacity_dose1: dose1, available_capacity_dose2: dose2} = sessions[i];
+    let {name, address, min_age_limit: age, fee, available_capacity_dose1: dose1, available_capacity_dose2: dose2, pincode} = sessions[i];
     
     if (age==ageSelectNode.value && (dose1>0 || dose2>0)) {
       
       class data {
-        constructor(name, address, age, fee, dose1, dose2) {
+        constructor(name, address, age, fee, dose1, dose2, pincode) {
           
           let cloneDiv = containerNode.cloneNode(true)
           
@@ -62,15 +61,15 @@ setTimeout(()=>{
           this.container = containerNode;
           
           this.container.querySelector('.name').innerText = name;
-          this.container.querySelector('.address').innerText = address;
-          this.container.querySelector('.age').innerText = `Age: ${age}`;
+          this.container.querySelector('.address').innerText = address+' '+pincode;
+          this.container.querySelector('.age').innerText = `Age: ${ageSelectNode.value}`;
           this.container.querySelector('.fee').innerText = `Fee: ${fee}`;
           this.container.querySelector('.dose1').innerText = `Dose 1: ${dose1}`;
           this.container.querySelector('.dose2').innerText = `Dose 2: ${dose2}`;
         }
       }
       
-      var node = new data(name, address, age, fee, dose1, dose2)
+      var node = new data(name, address, age, fee, dose1, dose2, pincode)
     }
   }
 }, 1000)
